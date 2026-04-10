beb901bc0f57eb23e89b78cce222549ced27693c
Tran Anh Kiet <trananhkiet980@gmail.com>
2026-04-10 16:28:11 +0700

    Fix Notifications Auto Clean

--- img2xl_app/views.py
+++ img2xl_app/views.py
@@ -583,6 +583,7 @@ def auto_cleanup_task(request):
     in_12h = now + timedelta(hours=12)
     trash_count = 0
     permanent_count = 0
+    warringDel_count = 0
 
     # --- PHẦN 1: DỌN DẸP CÁC FILE ĐÃ HẾT HẠN (QUÁ KHỨ) ---
     expired_files = UploadedFile.objects.filter(
@@ -605,21 +606,21 @@ def auto_cleanup_task(request):
             f.delete()  # Gọi hàm xóa vật lý nếu cần trước dòng này
             permanent_count += 1
 
-
     if trash_count > 0:
         # Thông báo cho người dùng về việc chuyển vào thùng rác
         Notification.objects.create_notification(
-            user=f.user,
+            user=request.user,
             title=u"Ảnh đã hết hạn!",
-            message=u"Ảnh '{}' đã được chuyển vào thùng rác và sẽ xóa vĩnh viễn sau 30 ngày.".format(f.filename),
+            message=u"Tự động dọn dẹp đã chuyển '{}' ảnh vào thùng rác và sẽ xóa vĩnh viễn sau 30 ngày.".format(trash_count),
             level='warning'
         )
+
     if permanent_count > 0:
         # Thông báo cho người dùng về việc xóa vĩnh viễn
         Notification.objects.create_notification(
-            user=user_storage,
+            user=request.ser,
             title=u"Xóa vĩnh viễn!",
-            message=u"Ảnh '{}' đã bị xóa vĩnh viễn khỏi hệ thống do hết hạn lưu trữ.".format(filename_storage),
+            message=u"Tự động dọn dẹp đã xoá '{}' ảnh vĩnh viễn khỏi hệ thống do hết hạn lưu trữ.".format(permanent_count),
             level='error'
         )
 
@@ -634,18 +635,19 @@ def auto_cleanup_task(request):
         from django.contrib.auth.models import User
         target_user = User.objects.get(id=entry['user'])
         count = entry['total']
+        warringDel_count += 1
 
-    # Tạo một thông báo tổng hợp duy nhất cho mỗi user để tránh spam
-    Notification.objects.create_notification(
-        user=target_user,
-        title=u"Sắp đến hạn xóa dữ liệu!",
-        message=u"Lưu ý: Bạn có {} ảnh sẽ bị xóa hoặc chuyển vào thùng rác trong vòng 12 giờ tới.".format(count),
-        level='info',
-        linked_to='/media-library/'  # Đường dẫn đến trang quản lý ảnh của bạn
-    )
+    if warringDel_count > 0:
+        # Tạo một thông báo tổng hợp duy nhất cho mỗi user để tránh spam
+        Notification.objects.create_notification(
+            user=request.user,
+            title=u"Sắp đến hạn xóa dữ liệu!",
+            message=u"Lưu ý: Bạn có {} ảnh sẽ bị xóa hoặc chuyển vào thùng rác trong vòng 12 giờ tới.".format(warringDel_count),
+            level='info',
+            linked_to='/documents/'  # Đường dẫn đến trang quản lý ảnh của bạn
+        )
 
     return HttpResponse(u"Đã hoàn thành dọn dẹp và gửi cảnh báo 12h.")
-
 
 
 def export(request, result_id):
