b14329cd2a39a7d54f864070c9d735abb8954e0a
Tran Anh Kiet <trananhkiet980@gmail.com>
2026-04-14 15:29:19 +0700

    Verify Email with Registration

--- img2xl_app/urls.py
+++ img2xl_app/urls.py
@@ -6,8 +6,13 @@ from . import views
 
 urlpatterns = [
     url(r'^register/$', views.register, name='register'),
+    url(r'^check-email/$', views.check_email_exists, name='check_email_exists'),
+    url(r'^send-otp/$', views.send_otp, name='send_otp'),
+    url(r'^verify-otp-ajax/$', views.verify_otp_ajax, name='verify_otp_ajax'),
     url(r'^login/$', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    url(r'^forgot-password/$', views.forgot_password_view, name='forgot_password'),
+    url(r'^reset-password-final/$', views.reset_password_final, name='reset_password_final'),
     url(r'^$', views.home, name='home'),
     url(r'^result/(?P<result_id>\d+)/$', views.result_detail, name='result_detail'),
     url(r'^delete/(?P<result_id>\d+)/$', views.delete_result, name='delete_result'),
